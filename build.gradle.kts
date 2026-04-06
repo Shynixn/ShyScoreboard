1dd00254aa6ed9a776af40d9c4b0fefe916a34bf
Shynixn <shynixn@email.com>
2026-04-06 22:48:55 +0200

    #46 Added support for Minecraft 26.1.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.github.shynixn"
-version = "1.13.0"
+version = "1.13.1"
 
 repositories {
     mavenCentral()
@@ -27,8 +27,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
     // Custom dependencies
-    implementation("com.github.shynixn.mcutils:common:2026.4")
-    implementation("com.github.shynixn.mcutils:packet:2026.9")
+    implementation("com.github.shynixn.mcutils:common:2026.5")
+    implementation("com.github.shynixn.mcutils:packet:2026.12")
     implementation("com.github.shynixn.mcutils:worldguard:2026.1")
 
     // Test
